808c9cba8c0ec07fbde5a98de4b64c1ea0eb76f0
Julia <julia.sarandi@gmail.com>
2022-01-25 00:17:32 -0800

    Optimize code.

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -4,7 +4,7 @@ import 'components/InterviewerList.scss'
 
 export default function InterviewerList(props) {
 
-  const dynamicInterviewerListItem = props.interviewers.map((interviewer) => {
+  const interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
         key={interviewer.id}
@@ -13,13 +13,13 @@ export default function InterviewerList(props) {
         selected={interviewer.id === props.interviewer}
         setInterviewer={() => props.setInterviewer(interviewer.id)}
       />
-    )
-  })
+    );
+  });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">{dynamicInterviewerListItem}</ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 };
