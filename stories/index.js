4d49adeeab1cffe5dfdd7d76d4739e43d4762a13
Julia <julia.sarandi@gmail.com>
2022-01-25 22:45:55 -0800

    Appointment components: Show.

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import InterviewerList from "../src/components/InterviewerList";
 import Appointment from "../src/components/Appointment";
 import Header from "../src/components/Appointment/Header";
 import Empty from "../src/components/Appointment/Empty";
+import Show from "../src/components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -141,4 +142,13 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment/>)
   .add("Appointment with Time", () => <Appointment time="12pm"/>)
   .add("Header", () => <Header time="12pm"/>)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => (
+    <Show
+      student={"Lydia Miller-Jones"}
+      interviewer={interviewers[0]}
+      onEdit={action("onEdit")}
+      onDelete={action("onDelete")}
+    />
+  ))
+;
